fa4b6b019de2d64e2da4d480029d2bdacd911567
Jason Heflinger <jasonheflinger@gmail.com>
2024-04-02 01:45:11 -0400

    update core

--- src/scenes/corescene.c
+++ src/scenes/corescene.c
@@ -2,14 +2,21 @@
 #include "core/logger.h"
 #include "raylib.h"
 
-CoreSceneState g_State    = CORE_NONE;
-Camera2D       g_Camera   = { 0 };
+CoreSceneState g_State            = CORE_NONE;
+Camera2D       g_Camera           = { 0 };
+Vector2        g_PlayerLocation   = { 0 };
+Vector2        g_PlayerSize       = { 20.0f, 20.0f };
 
 void DrawCoreScene() {
     BeginDrawing();
 
     ClearBackground(DARKGRAY);
-    DrawText("This is the CORE scene!", 190, 200, 20, GOLD);
+    
+	BeginMode2D(g_Camera);
+
+	DrawRectangleRec({g_PlayerLocation.x, g_PlayerLocation.y, g_PlayerSize.x, g_PlayerSize.y}, GOLD);
+
+	EndMode2D();
     
     EndDrawing();
 }
@@ -32,7 +39,7 @@ void UpdateCoreScene() {
 
 void InitializeCoreScene() {
 	// initialize camera
-    g_Camera.target = (Vector2){ 0.0f, 0.0f };
+    g_Camera.target = (Vector2){ g_PlayerLocation.x + g_PlayerSize.x/2.0f, g_PlayerLocation.y + g_PlayerSize.y/2.0f };
     g_Camera.offset = (Vector2){ GetScreenWidth()/2.0f, GetScreenHeight()/2.0f };
     g_Camera.rotation = 0.0f;
     g_Camera.zoom = 1.0f;
