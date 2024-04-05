caf551be7bb6a5cefa7c8d661cc657a8fb8d8e6a
Jason Heflinger <jasonheflinger@gmail.com>
2024-04-05 14:47:38 -0400

    added linux adaptation for ping

--- src/utils/timeutils.c
+++ src/utils/timeutils.c
@@ -2,6 +2,8 @@
 
 #ifdef _WIN32
 
+#include <Windows.h>
+
 uint32_t GetUnpreciseEpoch() {
     FILETIME ft;
     ULONGLONG epoch_time_ms;
@@ -12,8 +14,13 @@ uint32_t GetUnpreciseEpoch() {
 
 #elif __linux__
 
+#include <time.h>
+
 uint32_t GetUnpreciseEpoch() {
-    
+	struct timespec ts;
+	clock_gettime(CLOCK_REALTIME, &ts);
+    uint64_t epoch_time_ms = (uint64_t)ts.tv_sec * 1000 + (uint64_t)ts.tv_nsec / 1000000;
+	return epoch_time_ms - 0xffffffff;
 }
 
 #else
--- src/utils/timeutils.h
+++ src/utils/timeutils.h
@@ -3,16 +3,6 @@
 
 #include <stdint.h>
 
-#ifdef _WIN32
-
-#include <Windows.h>
-
-#elif __linux__
-
-#else
-#error "Unknown operating system!"
-#endif
-
 uint32_t GetUnpreciseEpoch();
 
 #endif
